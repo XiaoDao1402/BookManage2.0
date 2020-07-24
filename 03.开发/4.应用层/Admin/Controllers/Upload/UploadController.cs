fb6747be238955e60702e7600246043e8e8b97da
JQ-KF 01 <JQ-KF 01@PCOS-2001211348>
2020-07-24 14:40:27 +0800

    新增上传接口 by 何煜杰

---
+++ 03.开发/4.应用层/Admin/Controllers/Upload/UploadController.cs
@@ -0,0 +1,83 @@
+﻿using Admin.Model;
+using JW.Base.Http.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using JW.Base.Qiniuyun;
+using JW.Base.Qiniuyun.Config;
+using JW.Base.Configuration;
+
+namespace Admin.Controllers.Upload {
+    /// <summary>
+    /// 文件上传
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class UploadController : ControllerBase {
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public UploadController(){
+            QiniuConfig config = new QiniuConfig() {
+                AK = ConfigurationManager.Current.Configuration["Qiniu:AK"],
+                SK = ConfigurationManager.Current.Configuration["Qiniu:SK"],
+                Bucket = ConfigurationManager.Current.Configuration["Qiniu:Bucket"],
+                Domain = ConfigurationManager.Current.Configuration["Qiniu:Domain"],
+            };
+            QiniuConfigManager.Current.SetConfig(config);
+        }
+
+        /// <summary>
+        /// 上传文件接口
+        /// </summary>
+        /// <param name="fileType">文件类型 image 或者 video</param>
+        /// <param name="file">文件</param>
+        /// <returns></returns>
+        [SwaggerResponse(200, "文档注释", typeof(ApiModel<List<FileModel>>))]
+        [HttpPost]
+        public Task<IActionResult> Post(string fileType, IFormFile file) {
+            try {
+                FileModel result = new FileModel();
+
+                if (file.Length == 0) {
+                    return ApiModel.AsErrorResult<FileModel>(null, "上传文件错误请新选择");
+                }
+
+                Stream stream = file.OpenReadStream();
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+
+                string suffix = string.Empty;
+                suffix = Path.GetExtension(file.FileName);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                result.FileName = file.FileName;
+
+                switch (fileType) {
+                    case "image":
+                        Image image = Image.FromStream(stream);
+                        result.Url =  QiniuApiContext.Current.UploadApi.UploadByByte(bytes, suffix) + $"?{image.Width}x{image.Height}";
+                        break;
+                    case "video":
+                        result.Url = QiniuApiContext.Current.UploadApi.UploadByByte(bytes, suffix);
+                        break;
+                    default:
+                        return ApiModel.AsErrorResult(result, "上传文件类型未知");
+                }
+
+                return ApiModel.AsSuccessResult(result, "上传成功");
+            } catch (Exception ex) {
+                return ApiModel.AsExceptionResult(ex);
+            }
+        }
+
+    }
+}
---
+++ 03.开发/4.应用层/Admin/Model/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Model {
+    /// <summary>
+    /// 文件model
+    /// </summary>
+    public class FileModel {
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
